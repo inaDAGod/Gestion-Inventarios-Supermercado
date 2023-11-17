6c94c285f5da131894c20cfd13a63c0e40151d4f
jhessika-zarate <jhessikazarate@gmail.com>
2023-11-16 20:40:32 -0400

    Provedor

---
+++ src/inventariosSuper/Proveedor.java
@@ -0,0 +1,58 @@
+package inventariosSuper;
+
+public class Proveedor {
+	private String Id_Provedor;
+	private String nombre;
+	private String direccion;
+	private String telefono;
+    private String correoElectronico;
+	
+	public String getId_Provedor() {
+		return Id_Provedor;
+	}
+	
+	public void setId_Provedor(String id_Provedor) {
+		Id_Provedor = id_Provedor;
+	}
+	
+	public String getNombre() {
+		return nombre;
+	}
+	
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+	
+	public String getDireccion() {
+		return direccion;
+	}
+	
+	public void setDireccion(String direccion) {
+		this.direccion = direccion;
+	}
+	
+	public String getTelefono() {
+		return telefono;
+	}
+	
+	public void setTelefono(String telefono) {
+		this.telefono = telefono;
+	}
+	
+	public String getCorreoElectronico() {
+		return correoElectronico;
+	}
+	
+	public void setCorreoElectronico(String correoElectronico) {
+		this.correoElectronico = correoElectronico;
+	}
+
+	@Override
+	public String toString() {
+		return " Proveedor [Id=" + Id_Provedor + ", Nombre=" + nombre + ", Direccion=" + direccion
+				+ ", Telefono=" + telefono + ", Correo electronico=" + correoElectronico + "]";
+	}
+    
+	
+	
+}
