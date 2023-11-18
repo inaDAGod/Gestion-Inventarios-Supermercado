a38f88456bf9c9b8e2ff01774f3b883ec5fd70bc
Ansc0307 <143832391+Ansc0307@users.noreply.github.com>
2023-11-18 12:27:45 -0400

    nose2

--- src/inventariosSuper/Comprado.java
+++ src/inventariosSuper/Comprado.java
@@ -1,5 +1,34 @@
 package inventariosSuper;
-
+import java.time.LocalDateTime;
+import java.util.HashMap;
+import java.util.Map;
 public class Comprado {
 
+    private HashMap<Cliente, LocalDateTime> compras;
+
+    public Comprado() {
+        this.compras = new HashMap<>();
+    }
+
+    public void agregarCompra(Cliente cliente, LocalDateTime fecha) {
+        compras.put(cliente, fecha);
+    }
+
+    public LocalDateTime obtenerFechaDeCompra(Cliente cliente) {
+        return compras.getOrDefault(cliente, null);
+    }
+
+
+
+    public void mostrarCompras() {
+        System.out.println("Historial de compras:");
+        for (Map.Entry<Cliente, LocalDateTime> entry : compras.entrySet()) {
+            Cliente currentCliente = entry.getKey();
+            LocalDateTime fechaCompra = entry.getValue();
+
+            System.out.println("Cliente: " + currentCliente.getNombre() + ", Fecha: " + fechaCompra);
+            currentCliente.mostrarListaDeCompras(); // Assuming this method prints individual purchase details
+            System.out.println(); // Just for clarity between clients
+        }
+    }
 }
