17f1722d4c505d2a507d3ed7ba3a71e2177d6aec
Ansc0307 <143832391+Ansc0307@users.noreply.github.com>
2023-11-18 12:29:45 -0400

    nose 3

--- src/inventariosSuper/Compras.java
+++ src/inventariosSuper/Compras.java
@@ -1,5 +1,39 @@
 package inventariosSuper;
 
 public class Compras {
+    private Producto prod;
+    private int cant;
+    private double costoTotal;
 
+    public Compras(Producto prod, int cant) {
+        this.prod = prod;
+        this.cant = cant;
+        this.costoTotal = calcularCostoTotal();
+    }
+
+    public Producto getProd() {
+        return prod;
+    }
+
+    public void setProd(Producto prod) {
+        this.prod = prod;
+    }
+
+    public int getCant() {
+        return cant;
+    }
+
+    public void setCant(int cant) {
+        this.cant = cant;
+        this.costoTotal = calcularCostoTotal();
+    }
+
+    public double getCostoTotal() {
+        return costoTotal;
+    }
+
+    private double calcularCostoTotal() {
+        return this.prod.getPrecio() * this.cant;
+    }
 }
+
