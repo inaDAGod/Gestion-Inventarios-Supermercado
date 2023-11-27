fad0697740f3f5987b101bb6e4c0e07eca1fdea4
inaDAGod <danialee14@gmail.com>
2023-11-27 17:19:01 -0400

    mostrarTodoProveedores

--- src/inventariosSuper/Ventanas/ProveedorProductos.java
+++ src/inventariosSuper/Ventanas/ProveedorProductos.java
@@ -4,15 +4,27 @@ import java.awt.EventQueue;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.JScrollPane;
 import javax.swing.border.EmptyBorder;
 
 import inventariosSuper.Clases.Inventario;
+import inventariosSuper.Clases.Producto;
+import inventariosSuper.Clases.Proveedor;
+
 import java.awt.BorderLayout;
 import java.awt.Dimension;
 import javax.swing.JLabel;
 import java.awt.Font;
+import java.awt.GridLayout;
+import java.awt.Image;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
 import java.awt.Color;
 import javax.swing.JTextField;
+import javax.swing.BorderFactory;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
@@ -21,7 +33,7 @@ public class ProveedorProductos extends JFrame {
 	private Inventario inventario;
 	private JPanel contentPane;
 	private JTextField textField;
-
+	private JPanel panelTarjetas;
 	public ProveedorProductos(Inventario inventario) {
 		this.inventario = inventario;
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
@@ -60,12 +72,64 @@ public class ProveedorProductos extends JFrame {
 		panelBuscador.add(btnBuscar);
 		
 		JButton btnMostrarTodo = new JButton("MOSTRAR TODO");
+		btnMostrarTodo.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				mostrarProveedores(inventario.getProveedores(), panelTarjetas);
+			}
+		});
 		btnMostrarTodo.setFont(new Font("Times New Roman", Font.BOLD, 15));
 		btnMostrarTodo.setBounds(961, 150, 189, 23);
 		panelBuscador.add(btnMostrarTodo);
 		
-		JPanel panel = new JPanel();
-		contentPane.add(panel, BorderLayout.CENTER);
+		panelTarjetas = new JPanel();
+		JScrollPane scrollPane = new JScrollPane(panelTarjetas);
+        contentPane.add(scrollPane, BorderLayout.CENTER);
+		mostrarProveedores(inventario.getProveedores(), panelTarjetas);
 		
 	}
+	private void mostrarProveedores(ArrayList<Proveedor> proveedores, JPanel panelTarjetas) {
+		  panelTarjetas.removeAll();
+		 int numProveedor = proveedores.size();
+		    
+		    int filas = Math.max((int) Math.ceil((double) numProveedor / 4), 1);
+	        panelTarjetas.setLayout(new GridLayout(filas, 4, 10, 10));
+
+	        for (Proveedor proveedor : proveedores) {
+	            JButton buttonProvee = new JButton();
+	            buttonProvee.setLayout(new BorderLayout());
+
+	            String rutaImagen = "/imagenes/producto.png";
+	            ImageIcon iconProducto = new ImageIcon(ListaProductos.class.getResource(rutaImagen));
+	            Image imagenRedimensionada = iconProducto.getImage().getScaledInstance(100, 100, Image.SCALE_SMOOTH);
+	            ImageIcon iconRedimensionado = new ImageIcon(imagenRedimensionada);
+	            JLabel labelImagen = new JLabel(iconRedimensionado);
+	            buttonProvee.add(labelImagen, BorderLayout.CENTER);
+	            buttonProvee.addActionListener(new ActionListener() {
+	    			public void actionPerformed(ActionEvent e) {
+	    				
+	    			}
+	    		});
+
+	            JPanel panelDetalles = new JPanel();
+	            panelDetalles.setLayout(new GridLayout(0, 1));
+
+	            JLabel labelNombre = new JLabel("Nombre: " + proveedor.getNombre());
+	            JLabel labelDireccion= new JLabel("Direccion: " + proveedor.getDireccion());
+	            JLabel labelTelefono = new JLabel("Telefono: " + proveedor.getTelefono());
+	            JLabel labelCorreo = new JLabel("Correo: " + proveedor.getCorreoElectronico());
+
+	            panelDetalles.add(labelNombre);
+	            panelDetalles.add(labelDireccion);
+	            panelDetalles.add(labelTelefono);
+	            panelDetalles.add(labelCorreo);
+
+	            buttonProvee.add(panelDetalles, BorderLayout.SOUTH);
+
+	            buttonProvee.setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+	            panelTarjetas.add( buttonProvee);
+	        }
+	        panelTarjetas.revalidate();
+	        panelTarjetas.repaint();
+	    }
 }
